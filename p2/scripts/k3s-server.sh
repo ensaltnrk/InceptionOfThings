a880d14e09cfc5c84db8f8aaa2e18092abb3e121
Muhammed Karamuk <mkaramuk@proton.me>
2024-11-23 15:12:38 +0300

    feat: Remove unnecessary token generation for worker node

--- p2/scripts/k3s-server.sh
+++ p2/scripts/k3s-server.sh
@@ -5,6 +5,3 @@ export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --bind-address=192.168.56.1
 
 # Install latest k3s
 curl -sfL https://get.k3s.io | sh -
-
-# Save the token to the synced folder
-sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/agent-token.env
